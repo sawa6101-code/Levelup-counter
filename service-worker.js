370234c68546301dfebba4d8a1ce113d1a6df77f
sawa <sawai6101@yahoo.co.jp>
2026-08-16 09:33:10 +0900

    Refresh PWA cache for corrected rainbow apple icon

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='levelup-counter-v2';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg'];
+const CACHE='levelup-counter-v3';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-rainbow.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
